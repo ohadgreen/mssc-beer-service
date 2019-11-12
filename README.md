8a8aa46e0b2e37b8b4f9645a286a0b3cd7070af0
Ohad Green <ohad.green@odysii.com>
2019-11-12 16:59:10 +0200

    Added POJOs

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-MSSC Beer Service
+# MSSC Beer Service
 Spring Microservices example
---
+++ src/main/java/com/acme/msscbeerservice/web/model/BeerStyle.java
@@ -0,0 +1,5 @@
+package com.acme.msscbeerservice.web.model;
+
+public enum BeerStyle {
+    LAGER, PILSNER, STOUT, GOSE, PORTER, ALE, WHEAT, IPA, PARE_ALE
+}
